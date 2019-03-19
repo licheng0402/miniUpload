a1cf590c3fee6cecc8ca74c475bddc7610c8eb51
licheng <php_lee@163.com>
2019-03-19 10:14:39 +0800

    '发布测试'

---
+++ src/MyUpload.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/3/19
+ * Time: 10:11
+ */
+
+namespace MiniUpload;
+
+class MyUpload
+{
+    public function test(){
+        echo "欢迎你 miniupload!!";
+    }
+
+}
---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/3/19
+ * Time: 10:11
+ */
+require 'vendor/autoload.php';
+
+use MiniUpload\MyUpload as MyUpload;
+
+$miniUpload = new MyUpload();
+$miniUpload->test();
